904cb50daab44be41c30b5ab193281ecf7c18bec
Mariugo Megiani <mariugomegiani@gmail.com>
2023-03-31 10:40:12 -0300

    Project done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:jarvis/pages/widgets/avatar.dart';
-import 'package:jarvis/pages/widgets/chat_bubble.dart';
 import 'package:jarvis/pages/widgets/commands_container.dart';
 import 'package:jarvis/pages/widgets/commands_text.dart';
 import 'package:jarvis/service/openapi_service_impl.dart';
@@ -15,9 +17,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  SpeechToText speechToText = SpeechToText();
-  final OpenAPIServiceImpl openAPIService = OpenAPIServiceImpl();
+  final openAPIService = OpenAPIServiceImpl();
+  final flutterTts = FlutterTts();
+  final speechToText = SpeechToText();
   String mySpeech = '';
+  String? generatedContent;
+  String? generatedImageUrl;
+  int start = 200;
+  int delay = 200;
 
   @override
   void initState() {
@@ -25,11 +32,12 @@ class _HomePageState extends State<HomePage> {
     initSpeechToText();
   }
 
-  // @override
-  // void dispose() {
-  //   super.dispose();
-  //   speechToText.stop();
-  // }
+  @override
+  void dispose() {
+    speechToText.stop();
+    flutterTts.stop();
+    super.dispose();
+  }
 
   // Speech To Text Methods
   Future<void> initSpeechToText() async {
@@ -53,71 +61,149 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  // Text to Speech Methods
+  Future<void> systemSpeak(String speech) async {
+    await flutterTts.speak(speech);
+  }
+
+  Future<void> voiceAssistant() async {
+    if (speechToText.isNotListening) {
+      await startListening();
+    } else {
+      var speech = await openAPIService.isArtPrompt(mySpeech);
+      if (speech.contains('https')) {
+        generatedImageUrl = speech;
+        generatedContent = null;
+        setState(() {});
+      } else {
+        generatedImageUrl = null;
+        generatedContent = speech;
+        setState(() {});
+        await systemSpeak(speech);
+      }
+      await stopListening();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    var greeting = '';
+
+    if (DateTime.now().hour >= 5 && DateTime.now().hour < 12) {
+      greeting = 'Good morning';
+    } else if (DateTime.now().hour >= 12 && DateTime.now().hour < 18) {
+      greeting = 'Good afternoon';
+    } else {
+      greeting = 'Good evening';
+    }
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const Avatar(),
-              const ChatBubble(),
-              const CommandsText(),
-              Column(
-                children: [
-                  CommandsContainer(
-                    title: 'ChatGPT',
-                    description:
-                        'Ask questions and get smart answers powered by ChatGPT.',
-                    backgroundColor:
-                        Theme.of(context).colorScheme.primaryContainer,
-                    fontColor: Theme.of(context).colorScheme.onPrimaryContainer,
+              ZoomIn(
+                child: const Avatar(),
+              ),
+              Visibility(
+                visible: generatedImageUrl == null,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
                   ),
-                  CommandsContainer(
-                    title: 'Dall-E',
-                    description:
-                        'Get inspiring and unique pictures powered by Dall-E.',
-                    backgroundColor:
-                        Theme.of(context).colorScheme.secondaryContainer,
-                    fontColor:
-                        Theme.of(context).colorScheme.onSecondaryContainer,
+                  margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+                    top: 30,
                   ),
-                ],
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                    borderRadius: BorderRadius.circular(20).copyWith(
+                      topLeft: Radius.zero,
+                    ),
+                  ),
+                  child: generatedContent == null
+                      ? AnimatedTextKit(
+                          isRepeatingAnimation: false,
+                          repeatForever: false,
+                          animatedTexts: [
+                            TyperAnimatedText(
+                              '$greeting, how can I help you today?',
+                              textStyle: TextStyle(
+                                fontSize: generatedContent == null ? 25 : 18,
+                                color: Theme.of(context).colorScheme.onSurface,
+                              ),
+                            ),
+                          ],
+                        )
+                      : FadeInRight(
+                          child: Text(
+                            '$generatedContent',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Theme.of(context).colorScheme.onSurface,
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+              if (generatedImageUrl != null)
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(generatedImageUrl!),
+                  ),
+                ),
+              Visibility(
+                visible: generatedContent == null && generatedImageUrl == null,
+                child: const CommandsText(),
+              ),
+              Visibility(
+                visible: generatedContent == null && generatedImageUrl == null,
+                child: Column(
+                  children: [
+                    SlideInUp(
+                      delay: Duration(milliseconds: start),
+                      child: CommandsContainer(
+                        title: 'ChatGPT',
+                        description:
+                            'Ask questions and get smart answers powered by ChatGPT.',
+                        backgroundColor:
+                            Theme.of(context).colorScheme.primaryContainer,
+                        fontColor:
+                            Theme.of(context).colorScheme.onPrimaryContainer,
+                      ),
+                    ),
+                    SlideInUp(
+                      delay: Duration(milliseconds: start + delay),
+                      child: CommandsContainer(
+                        title: 'Dall-E',
+                        description:
+                            'Get inspiring and unique pictures powered by Dall-E.',
+                        backgroundColor:
+                            Theme.of(context).colorScheme.secondaryContainer,
+                        fontColor:
+                            Theme.of(context).colorScheme.onSecondaryContainer,
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).colorScheme.tertiary,
-        foregroundColor: Theme.of(context).colorScheme.onTertiary,
-        onPressed: () async {
-          if (speechToText.isNotListening) {
-            await startListening();
-          } else {
-            var speech = await openAPIService.isArtPrompt(mySpeech);
-            print('========= SPEECH: $speech ==============');
-            await stopListening();
-          }
-          //speechToText.isNotListening ? startListening() : stopListening();
-
-          // print('================ ENTROU NO BUTTON ==============');
-          // if (await speechToText.hasPermission && speechToText.isNotListening) {
-          //   print(
-          //       '================ ENTROU NO haspermisson isnotlistening ==============');
-          //   await startListening();
-          // } else if (speechToText.isListening) {
-          //   print('================ ENTROU NO islistening ==============');
-
-          //   //String speech = await openAPIService.isArtPrompt(mySpeech);
-          //   //print('================ SPEECHRESULT $speech ==============');
-          //   await stopListening();
-          // } else {
-          //   initSpeechToText();
-          // }
-        },
-        child: Icon(
-          speechToText.isNotListening ? Icons.mic : Icons.mic_off,
+      floatingActionButton: ZoomIn(
+        delay: Duration(milliseconds: start + (2 * delay)),
+        child: FloatingActionButton(
+          backgroundColor: Theme.of(context).colorScheme.tertiary,
+          foregroundColor: Theme.of(context).colorScheme.onTertiary,
+          onPressed: () async => await voiceAssistant(),
+          child: Icon(
+            speechToText.isNotListening ? Icons.mic : Icons.stop,
+          ),
         ),
       ),
     );
